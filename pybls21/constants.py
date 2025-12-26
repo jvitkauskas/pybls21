53df0513f4559b004d474739a4bef89c31472764
LuederBosse <76811388+LuederBosse@users.noreply.github.com>
2025-12-26 15:24:29 +0100

    Update constants.py
    extentions from MaNi and Lüder

--- pybls21/constants.py
+++ pybls21/constants.py
@@ -1,7 +1,9 @@
 # Coils
 CL_POWER = 0
 CL_Boost_MODE = 3
+CL_BoostSWITCH_CTRL = 13  # Input activation for the boost mode switch # Lüder
 CL_RESET_FILTER_TIMER = 17
+CL_RESET_ALARM = 18 # Reset all alarms # Lüder
 
 # Holding registers
 HR_MaxSPEED_MODE = 1
@@ -19,3 +21,10 @@ IR_VerMAIN_FMW_start = 34
 IR_VerMAIN_FMW_end = 36
 IR_DeviceTYPE = 37
 IR_ALARM = 38
+
+# MaNi additions
+IR_CurTEMP_ExAirIn = 1 # temperature for ingoing fresh air
+IR_CurTEMP_ExAirOut = 2 # temperature for outgoing air
+IR_CurFILTER_TIMER = 1 # countdown until filters needs cleaning or change
+IR_CurSuPRESS = 100 # air pressure for ingoing fresh air
+IR_CurExPRESS = 100 # air pressure for outgoing air
